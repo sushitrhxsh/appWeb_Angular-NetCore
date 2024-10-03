2969a5f167b2562d1bafc6ab6c4f1b66c172f0fb
alex.none <alexfdzs12@gmail.com>
2024-10-03 13:05:14 -0500

    Agregamos y corregimos el fronted desde AppSistVentAN en DashBoard component del cual tocamos css, html y ts, falta por corregir el guardar las ventas c15

--- ApiSistVentAN/SistVentAN.DAL/obj/Debug/net7.0/SistVentAN.DAL.AssemblyInfo.cs
+++ ApiSistVentAN/SistVentAN.DAL/obj/Debug/net7.0/SistVentAN.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistVentAN.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd32512a4cf97636ac9b426a07b6b59db1342cd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503ddcf0a0954dea3c4e571dff7720475d4d4182")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistVentAN.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistVentAN.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AppSistVentAN/src/app/Components/layout/Pages/dash-board/dash-board.component.ts
+++ AppSistVentAN/src/app/Components/layout/Pages/dash-board/dash-board.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Chart,registerables } from 'chart.js';
+import { DashBoardService } from 'src/app/Services/dash-board.service';
+Chart.register(...registerables);
+
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashBoardComponent implements OnInit {
 
-  constructor() { }
+  totalIngresos:string  = "0";
+  totalVentas:string    = "0";
+  totalProductos:string = "0";
+  
+
+  constructor(
+    private _dashboardService:DashBoardService
+  ){ }
+
+  mostrarGrafico(labelGrafico:any[], dataGrafico:any[]){
+    const chartBar = new Chart('chartBar', {
+      type: "bar",
+      data: {
+        labels: labelGrafico,
+        datasets: [{
+          label:           "Numero de ventas",
+          data:            dataGrafico,
+          backgroundColor: ["rgba(211, 89, 222, 0.8)"],
+          borderColor:     ["purple"],
+          borderWidth:     1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive:          true,
+        scales: {
+          y: { beginAtZero: true }
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this._dashboardService.resumen().subscribe({
+      next:(data) => {
+        if(data.status){
+          this.totalIngresos  = data.value.totalIngresos;
+          this.totalVentas    = data.value.totalVentas;
+          this.totalProductos = data.value.totalProductos;
+          
+          const arrayData:any[] = data.value.ventasUltimaSemana;
+          console.log(arrayData);
+
+          const labelTemp = arrayData.map((v) => v.fecha);
+          const dataTemp  = arrayData.map((v) => v.total);
+          console.log(labelTemp,dataTemp);
+
+          this.mostrarGrafico(labelTemp,dataTemp);
+        }
+      },
+      error:(e) => { }
+    });
   }
 
 }
